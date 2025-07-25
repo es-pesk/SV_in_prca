801c574fb6650b679a489d11a9d3d526d7695ae2
es-pesk <168341077+es-pesk@users.noreply.github.com>
2025-07-26 02:30:55 +0300

    Update segment_cov_short.py

--- segment_cov_short.py
+++ segment_cov_short.py
@@ -55,13 +55,16 @@ def process_chrom(args):
         start = prev = sig_pos[0]
         for p1 in sig_pos[1:]:
             if p1 - prev > max_gap:
-                #seg.append(f"{chrom}\t{start}\t{prev+1}")
-                #start = p1
-                seg_z = np.nanmean(z[(pos >= start) & (pos <= prev)])  #  средний Z-score по сегменту
-                seg.append(f\"{chrom}\\t{start}\\t{prev+1}\\t{seg_z:.6f}\") # записываем в .bed 4м столбцом
-                
+                seg_z = np.nanmean(z[(pos >= start) & (pos <= prev)])  # средний Z по сегменту
+                seg.append(f\"{chrom}\\t{start}\\t{prev+1}\\t{seg_z:.6f}\") # средний Z по сегменту 4м столбцом в bed
+                start = p1
             prev = p1
-        seg.append(f"{chrom}\t{start}\t{prev+1}")
+                
+        # финальный сегмент
+        seg_z = np.nanmean(z[(pos >= start) & (pos <= prev)])
+        seg.append(f\"{chrom}\\t{start}\\t{prev+1}\\t{seg_z:.6f}\")
+                   
+        #seg.append(f"{chrom}\t{start}\t{prev+1}")
 
 
 
